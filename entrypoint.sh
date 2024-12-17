c0b5c0034c581514cbccfe1d7f9805fb60150abd
Valentin Sene <pro@valentinsene.me>
2024-12-17 11:38:18 +0100

    Remove debug print

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,5 @@ echo "{\"endpoint\":\"https://porkbun.com/api/json/v3\",\"secretapikey\":\"${SEC
 javac -cp "./libs/httpcomponents-client-5.0.3/lib/httpclient5-5.0.3.jar:./libs/httpcomponents-client-5.0.3/lib/httpcore5-5.0.2.jar:./libs/httpcomponents-client-5.0.3/lib/slf4j-api-1.7.25.jar:./libs/json.jar" -d . PorkbunDynDNSClient.java
 make jar
 
-echo "---" && pwd && ls && cat config.json && echo "---"
-
 # Run the jar file
 java -jar porkbun-ddns.jar ${DOMAIN} ${SUBDOMAIN} A
