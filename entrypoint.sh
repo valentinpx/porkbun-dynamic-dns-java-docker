e560ab6f97bf45b10e97ca2e1f75d5d854a8fa04
Valentin Sene <pro@valentinsene.me>
2025-03-06 11:46:17 +0100

    Updated build command

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 echo "{\"endpoint\":\"https://porkbun.com/api/json/v3\",\"secretapikey\":\"${SECRET_API_KEY}\",\"apikey\":\"${API_KEY}\"}" > config.json
 
 # Build the jar file
-javac -cp "./libs/httpcomponents-client-5.0.3/lib/httpclient5-5.0.3.jar:./libs/httpcomponents-client-5.0.3/lib/httpcore5-5.0.2.jar:./libs/httpcomponents-client-5.0.3/lib/slf4j-api-1.7.25.jar:./libs/json.jar" -d . PorkbunDynDNSClient.java
+make
 make jar
 
 # Run the jar file
